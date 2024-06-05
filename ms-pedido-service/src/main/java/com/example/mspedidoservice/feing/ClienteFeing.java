82d420567c752593ca4ce34bc8d1d6d8a59432f0
YeralD21 <yesarbot1@gmail.com>
2024-06-05 10:34:56 -0500

    Resiliencia en microservicios

--- ms-pedido-service/src/main/java/com/example/mspedidoservice/feing/ClienteFeing.java
+++ ms-pedido-service/src/main/java/com/example/mspedidoservice/feing/ClienteFeing.java
@@ -1,6 +1,7 @@
 package com.example.mspedidoservice.feing;
 
 import com.example.mspedidoservice.dto.ClienteDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "ms-clientegym-service", path = "/clientegym")
 public interface ClienteFeing {
     @GetMapping("/{id}")
-    ResponseEntity<ClienteDto> findById(@PathVariable(required = true) Integer id);
+    @CircuitBreaker(name = "clienteListarPorIdCB", fallbackMethod = "fallbackClientePorId")
+    public ResponseEntity<ClienteDto> findById(@PathVariable(required = true) Integer id);
+    default ResponseEntity<ClienteDto> fallbackClientePorId(Integer id, Exception e) {
+        return ResponseEntity.ok(new ClienteDto());
+    }
 }
