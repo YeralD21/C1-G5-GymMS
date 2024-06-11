ba34a93c943ea1c9b03336ea3e7c33bfbdae9835
YeralD21 <yesarbot1@gmail.com>
2024-06-10 22:24:46 -0500

    Feing

--- ms-asesoramiento/src/main/java/com/example/msasesoramiento/entity/Asesoramiento.java
+++ ms-asesoramiento/src/main/java/com/example/msasesoramiento/entity/Asesoramiento.java
@@ -1,9 +1,6 @@
 package com.example.msasesoramiento.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import java.util.Date;
 
@@ -23,5 +20,7 @@ public class Asesoramiento {
 
     private Integer clientegymId;
     private Integer trainerId;
+
+
 
 }
