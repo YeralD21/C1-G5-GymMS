284decd4ffaa2c4fd9e948aea725b62c997f59eb
AlexCoilaJrt <104539425+AlexCoilaJrt@users.noreply.github.com>
2024-06-11 23:43:57 -0500

    clase res

--- ms-clase/src/main/java/com/example/msclase/feing/InstructorFeing.java
+++ ms-clase/src/main/java/com/example/msclase/feing/InstructorFeing.java
@@ -1,6 +1,7 @@
 package com.example.msclase.feing;
 
 import com.example.msclase.dto.InstructorDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,5 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 public interface InstructorFeing {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "clienteclassListarPorId", fallbackMethod = "fallbackClientePorId")
     public ResponseEntity<InstructorDto> buscarPorId(@PathVariable(required = true)Integer id);
+    default ResponseEntity<InstructorDto> fallbackClientePorId(Integer id, Exception e) {
+        return ResponseEntity.ok(new InstructorDto());
+    }
 }
