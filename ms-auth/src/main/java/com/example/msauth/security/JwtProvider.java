179709cb1fe67238ae246150187cb6c64ab1716b
AlexCoilaJrt <104539425+AlexCoilaJrt@users.noreply.github.com>
2024-06-11 20:14:25 -0500

    auth

--- ms-auth/src/main/java/com/example/msauth/security/JwtProvider.java
+++ ms-auth/src/main/java/com/example/msauth/security/JwtProvider.java
@@ -5,25 +5,19 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
-
 @Component
-public class JwtProvider {
+public class JwtProvider    {
     @Value("${jwt.secret}")
     private String secret;
-
-
     @PostConstruct
     protected void init() {
         secret = Base64.getEncoder().encodeToString(secret.getBytes());
     }
-
 
     public String createToken(AuthUser authUser) {
         Map<String, Object> claims = new HashMap<>();
@@ -39,22 +33,20 @@ public class JwtProvider {
                 .compact();
     }
 
-
     public boolean validate(String token) {
         try {
             Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
 
             return true;
-        } catch (Exception e) {
+        }catch (Exception e){
             return false;
         }
     }
 
-
-    public String getUserNameFromToken(String token) {
+    public String getUserNameFromToken(String token){
         try {
             return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
-        } catch (Exception e) {
+        }catch (Exception e) {
             return "bad token";
         }
     }
--- ms-auth/src/main/java/com/example/msauth/security/PasswordEncoderConfig.java
+++ ms-auth/src/main/java/com/example/msauth/security/PasswordEncoderConfig.java
@@ -1,10 +1,10 @@
 package com.example.msauth.security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 @Configuration
 public class PasswordEncoderConfig {
     @Bean
