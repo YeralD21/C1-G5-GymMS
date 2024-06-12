be98d0f82b82274ef6f64cdb61d6e2cc3f85ce44
AlexCoilaJrt <104539425+AlexCoilaJrt@users.noreply.github.com>
2024-06-12 08:24:22 -0500

    resilience and load balance

--- ms-pagoclase/src/main/java/com/example/mspagoclase/feing/ClaseFeing.java
+++ ms-pagoclase/src/main/java/com/example/mspagoclase/feing/ClaseFeing.java
@@ -2,6 +2,7 @@ package com.example.mspagoclase.feing;
 
 import com.example.mspagoclase.dto.ClaseDto;
 import com.example.mspagoclase.dto.ClientepagoclaseDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,5 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ClaseFeing {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "clasePorId", fallbackMethod = "fallbackClasePorId")
     public ResponseEntity<ClaseDto> buscarPorId(@PathVariable(required = true)Integer id);
+    default ResponseEntity<ClaseDto> fallbackClasePorId(Integer id, Exception e) {
+        return ResponseEntity.ok(new ClaseDto());
+    }
 }
