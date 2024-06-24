a7b424d55717ac2491a626eefc9b26d0233fb9f4
YeralD21 <yesarbot1@gmail.com>
2024-06-24 15:54:37 -0500

    main

--- app_vin/src/app/components/unauthorized/unauthorized.component.ts
+++ app_vin/src/app/components/unauthorized/unauthorized.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-unauthorized',
-    template: '<h1>No estás autorizado para ver esta página</h1>',
-    styles: ['h1 { color: red; }']
+    template: '<h1>Unauthorized</h1>',
+    standalone: true
 })
-export class UnauthorizedComponent { }
+export class UnauthorizedComponent {}
