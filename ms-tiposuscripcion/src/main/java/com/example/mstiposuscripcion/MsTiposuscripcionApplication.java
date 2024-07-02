05678f68fc78848ae96bcc8eb0e5f726b6b3d266
YeralD21 <yesarbot1@gmail.com>
2024-07-01 22:32:20 -0500

    ms-inscripcion

---
+++ ms-tiposuscripcion/src/main/java/com/example/mstiposuscripcion/MsTiposuscripcionApplication.java
@@ -0,0 +1,13 @@
+package com.example.mstiposuscripcion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTiposuscripcionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTiposuscripcionApplication.class, args);
+	}
+
+}
