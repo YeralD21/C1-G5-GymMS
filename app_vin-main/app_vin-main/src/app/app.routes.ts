3f0c2e14a4667f20cd876ea6795250a23ffc9272
YeralD21 <yesarbot1@gmail.com>
2024-07-04 01:49:13 -0500

    s

---
+++ app_vin-main/app_vin-main/src/app/app.routes.ts
@@ -0,0 +1,128 @@
+import { Route } from '@angular/router';
+import { initialDataResolver } from 'app/app.resolvers';
+import { LayoutComponent } from 'app/layout/layout.component';
+import { AuthGuard } from './providers/guards/auth.guard';
+import { NoAuthGuard } from './providers/guards/noAuth.guard';
+
+export const appRoutes: Route[] = [
+    // Redirect empty path to '/example'
+    { path: '', pathMatch: 'full', redirectTo: 'example' },
+    // { path: '/role', pathMatch: 'full', redirectTo: 'app-role' },
+
+    // Redirect signed-in user to the '/example'
+    { path: 'signed-in-redirect', pathMatch: 'full', redirectTo: 'example' },
+
+    // Auth routes for guests
+    {
+        path: '',
+        canActivate: [NoAuthGuard],
+        canActivateChild: [NoAuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty',
+        },
+        children: [
+            {
+                path: 'confirmation-required',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/confirmation-required/confirmation-required.routes'
+                        ),
+            },
+            {
+                path: 'forgot-password',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/forgot-password/forgot-password.routes'
+                        ),
+            },
+            {
+                path: 'reset-password',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/reset-password/reset-password.routes'
+                        ),
+            },
+            {
+                path: 'sign-in',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-in/sign-in.routes'),
+            },
+            {
+                path: 'sign-up',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-up/sign-up.routes'),
+            },
+        ],
+    },
+    // {
+    //     path: '',
+    //     component: LayoutComponent,
+    //     data: {
+    //         layout: 'classic'
+    //     },
+    //     children:[
+    //         {path: 'homeScreen', loadChildren: () => import('app/views/views.routes')},
+    //     ]
+    // },
+
+    // Auth routes for authenticated users
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty',
+        },
+        children: [
+            {
+                path: 'sign-out',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-out/sign-out.routes'),
+            },
+            {
+                path: 'unlock-session',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/unlock-session/unlock-session.routes'
+                        ),
+            },
+        ],
+    },
+
+    // Landing routes
+    {
+        path: '',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty',
+        },
+        children: [
+            {
+                path: 'home',
+                loadChildren: () =>
+                    import('app/modules/landing/home/home.routes'),
+            },
+        ],
+    },
+
+    // Admin routes
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        resolve: {
+            initialData: initialDataResolver,
+        },
+        children: [
+            { path: 'example', loadChildren: () => import('app/modules/admin/example/example.routes') },
+            { path: 'tree', loadChildren: () => import('app/modules/admin/crud-tree/crud-tree.routes') },
+            { path: 'homeScreen', loadChildren: () => import('app/views/views.routes') },
+            // Ruta para Setup Instructor
+            { path: 'homeScreen/setup/instructor', loadChildren: () => import('app/views/dashboard/setup/instructor/instructor.routers').then(m => m.default) },
+
+        ]
+    },
+];
